60c67473f2078da856a3aef1a1021a9b31037451
william lamptey <williamlamp512@gmail.com>
2024-06-05 00:31:42 +0000

    building temperature tile

--- weather_app/lib/pages/weather.dart
+++ weather_app/lib/pages/weather.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class WeatherUi extends StatefulWidget {
   const WeatherUi({super.key});
@@ -10,8 +11,37 @@ class WeatherUi extends StatefulWidget {
 class _WeatherUiState extends State<WeatherUi> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromRGBO(245, 245, 245, 1),
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F5F5),
+
+      body: SafeArea(
+       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+         Container(
+          decoration: const BoxDecoration(
+            color: Colors.white
+          ),
+          padding: const EdgeInsets.all(20),
+          height: 147,
+          width: 390,
+          child: const Row(
+            children: [
+              Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Today'),
+                  Text('Accra, Ghana'),
+                  Text('24th Jun')
+                ],
+              )
+            ],
+          ),
+         )
+        ],
+        
+       ),
+      ),
     );
   }
 }
