870aa2ba2baceff962188673bc1b3c8f94ee7c10
william lamptey <williamlamp512@gmail.com>
2024-06-08 13:24:16 +0000

    current weather list tile Ui completed

--- weather_app/lib/pages/weather.dart
+++ weather_app/lib/pages/weather.dart
@@ -44,48 +44,66 @@ Widget weatherTile(cityName){
 return Container(
   width: 400,
   height: 300,
-  decoration: const BoxDecoration(
+  decoration: BoxDecoration(
     color: Colors.white,
+    borderRadius: BorderRadius.circular(20),
+    boxShadow: const [
+      BoxShadow(
+      offset: Offset(0, 1),
+      blurRadius: 1,
+      color: Color.fromRGBO(0, 0, 0, 0.2),
+    )
+    ]
   ),
   child: Padding(
-    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+    padding: const EdgeInsets.all(8),
     child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Today',
-                  style: GoogleFonts.poppins(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: const Color(0xFF828282),
-                  ),
-                  ),
-                Text(
-                  '$cityName, $country',
-                  style: GoogleFonts.poppins(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: const Color(0xFF333333)
-                  ),
-                  ),
-                Text(
-                  '${month[monthnum]} $day | Condition',
-                  style: GoogleFonts.poppins(
-                    fontSize: 16,
-                    fontWeight: FontWeight.normal,
-                    color: const Color(0xFF828282),
-                  ),
-                  )
-              ],
-            ),
-            const FlutterLogo( size: 80,),
-          ],
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Today',
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                        color: const Color(0xFF828282),
+                      ),
+                      ),
+                    Text(
+                      '$cityName, $country',
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0xFF333333)
+                      ),
+                      ),
+                    Text(
+                      '${month[monthnum]} $day | Mostly Cloudy',
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        color: const Color(0xFF828282),
+                      ),
+                      )
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child:  FlutterLogo( size: 80,),
+              ),
+            ],
+          ),
         ),
         //change to fit temperature
         Text(
@@ -95,18 +113,30 @@ return Container(
             fontWeight: FontWeight.w500,
           ),
           ),
-          const Padding(
+           Padding(
             padding: const EdgeInsets.all(10),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('H:32°'),
-                SizedBox(width: 20,),
-                Text('L:24°')
+                Text(
+                  'H:32°',
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    color:const Color(0xFF828282)
+                  ),
+                  ),
+                const SizedBox(width: 20,),
+                Text('L:24°',
+                 style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    color:const Color(0xFF828282)
+                  ),
+                )
+    
               ],
             ),
           )
-
+    
       ],
     ),
   ),
