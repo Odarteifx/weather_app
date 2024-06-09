cad02fa57e6de91fd79c1041caaececb809512b1
william lamptey <williamlamp512@gmail.com>
2024-06-09 09:59:31 +0000

    Added Weather icon

--- weather_app/lib/pages/weather.dart
+++ weather_app/lib/pages/weather.dart
@@ -21,7 +21,7 @@ late final String day ;
 class _WeatherUiState extends State<WeatherUi> {
 
 WeatherRequest wr = WeatherRequest('d2c2d6e65f574c52b0d224818240606', language: Language.english);
-late final String cityName = 'Miami';
+late final String cityName = 'Sunyani';
   
 
 RealtimeWeather? weatherData;
@@ -79,6 +79,7 @@ bool isloading = true;
 }
 
 Widget weatherTile(weather){
+  String weathData = '${month[monthnum]} $day | ${weather.current.condition.text.toString()}';
 return Container(
   width: 400,
   height: 300,
@@ -106,39 +107,45 @@ return Container(
             children: [
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Today',
-                      style: GoogleFonts.poppins(
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                        color: const Color(0xFF828282),
-                      ),
-                      ),
-                    Text(
-                      '${weather.location.name}',
-                      style: GoogleFonts.poppins(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                        color: const Color(0xFF333333)
-                      ),
-                      ),
-                    Text(
-                      '${month[monthnum]} $day | ${weather.current.condition.text.toString()}',
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                        fontWeight: FontWeight.normal,
-                        color: const Color(0xFF828282),
-                      ),
-                      )
-                  ],
+                child: Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Today',
+                        style: GoogleFonts.poppins(
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                          color: const Color(0xFF828282),
+                        ),
+                        ),
+                      Text(
+                        '${weather.location.name}',
+                        style: GoogleFonts.poppins(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: const Color(0xFF333333)
+                        ),
+                        ),
+                      Text(
+                        weathData,
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: const Color(0xFF828282),
+                        ),
+                        )
+                    ],
+                  ),
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                child: FlutterLogo(size: 80,),
+               Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                child: Image.network(
+                  'https:${weather.current.condition.icon}',
+                  width: 80,
+                  height: 80,
+                  ),
               ),
             ],
           ),
